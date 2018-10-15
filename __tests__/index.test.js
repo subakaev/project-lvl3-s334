dca3381d423bbf56fc6e903dd5d993e81f0da250
Sergey Bakaev <bakaev@yandex.ru>
2018-10-15 20:48:36 +0300

    fixture and failed test for simple pageLoader

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,30 @@
+import nock from 'nock';
+import axios from 'axios';
+import httpAdapter from 'axios/lib/adapters/http';
+import path from 'path';
+import fs from 'fs';
+import os from 'os';
+
 import pageLoader from '../src';
 
-test('pageLoader test', () => {
+const fixturesDir = '__tests__/__fixtures__';
+const url = 'http://example.com';
+
+beforeAll(() => {
+  axios.defaults.adapter = httpAdapter;
+});
+
+test('pageLoader test', async () => {
+  const expectedContent = fs.readFileSync(path.join(fixturesDir, 'example-com.html'));
+  const tempDir = path.join(os.tmpdir(), 'pageLoaderTmp');
+
+  fs.mkdtempSync(tempDir);
+
+  nock(url).get('/').reply(200, expectedContent);
+
+  await pageLoader(url, tempDir);
+
+  const actualData = fs.readFileSync(path.join(tempDir, 'example-com.html'));
+
+  expect(actualData).toEqual(expectedContent);
 });

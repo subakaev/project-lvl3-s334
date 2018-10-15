cd53e3f2755aec20c1b94267a586dcab7e0762dc
Sergey Bakaev <bakaev@yandex.ru>
2018-10-15 19:38:05 +0300

    base configuration for commander

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('hello');
+import program from 'commander';
+
+program
+  .version('0.0.1')
+  .description('The utility downloads the page and saves it to the local folder.')
+  .arguments('<address>')
+  .option('--output [path]', 'Output path')
+  .action((address, cmd) => {
+    console.log(address);
+    console.log(cmd.output);
+  })
+  .parse(process.argv);

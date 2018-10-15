b5a7a1a0bebdf6f46ecc50259fb40e7c4a2e5750
Sergey Bakaev <bakaev@yandex.ru>
2018-10-15 20:06:01 +0300

    getFolderNameFromUrl implementation

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,12 @@
 import url from 'url';
 
 const getFolderNameFromUrl = (urlString) => {
-  return '';
+  const { protocol } = url.parse(urlString);
+
+  return urlString
+    .slice(`${protocol}//`.length)
+    .replace(/[^A-Za-z0-9]/g, '-')
+    .concat('.html');
 };
 
 export default getFolderNameFromUrl;

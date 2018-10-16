b2066223e3d2e27b134f9ea961f296998474b671
Sergey Bakaev <bakaev@yandex.ru>
2018-10-16 19:06:09 +0300

    add test for html modification

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -16,7 +16,7 @@ beforeAll(() => {
   axios.defaults.adapter = httpAdapter;
 });
 
-test('pageLoader test', async () => {
+test('page-loader should download without resources', async () => {
   const expectedContent = await fsPromises.readFile(path.join(fixturesDir, 'example-com.html'));
   const tempDir = await fsPromises.mkdtemp(path.join(os.tmpdir(), 'pageLoaderTmp-'));
 
@@ -29,4 +29,32 @@ test('pageLoader test', async () => {
   const actualData = await fsPromises.readFile(path.join(tempDir, 'example-com.html'));
 
   expect(actualData).toEqual(expectedContent);
+});
+
+test('page-loader should download with resources', async () => {
+  const inputHtmlPath = path.join(fixturesDir, 'example-com-contents-source.html');
+  const inputHtml = await fsPromises.readFile(inputHtmlPath);
+
+  const expectedHtmlPath = path.join(fixturesDir, 'example-com-contents.html');
+  const expectedHtml = await fsPromises.readFile(expectedHtmlPath);
+
+  const tempDir = await fsPromises.mkdtemp(path.join(os.tmpdir(), 'pageLoaderTmp-'));
+
+  nock(host)
+    .get('/')
+    .reply(200, inputHtml)
+    .get('/styles/style.css')
+    .reply(200, {})
+    .get('/assets/script-in-head.js')
+    .reply(200, {})
+    .get('/assets/script-in-body.js')
+    .reply(200, {})
+    .get('/img/picture.png')
+    .reply(200, {});
+
+  await pageLoader(host, tempDir);
+
+  const actualHtml = await fsPromises.readFile(path.join(tempDir, 'example-com.html'));
+
+  expect(actualHtml).toEqual(expectedHtml);
 });
